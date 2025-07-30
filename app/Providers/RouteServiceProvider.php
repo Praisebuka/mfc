26f789355c9da47b17fa70c6431b3d576ccd20ed
Praisebuka <praise.njoga@gmail.com>
2025-07-30 15:23:19 +0100

    tryna config api path  but it didn't later work

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -12,11 +12,8 @@ class RouteServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        Route::middleware('api')
-            ->prefix('api')
-            ->group(base_path('routes/api.php'));
+        Route::middleware('api')->prefix('api')->group(base_path('routes/api.php'));
 
-        Route::middleware('web')
-            ->group(base_path('routes/web.php'));
+        Route::middleware('web')->group(base_path('routes/web.php'));
     }
 }
