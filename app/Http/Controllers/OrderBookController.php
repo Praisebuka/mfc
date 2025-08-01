8bd214e5da55546b2f08cf6f87b9e539fc40f72e
Praisebuka <praise.njoga@gmail.com>
2025-08-01 07:19:12 +0100

    added presale for local ordering config

--- app/Http/Controllers/OrderBookController.php
+++ app/Http/Controllers/OrderBookController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\OrderPresale;
+use App\Models\OrderPresaleForLocal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Throwable;
 
 class OrderBookController extends Controller
@@ -21,12 +24,24 @@ class OrderBookController extends Controller
     public function localOrdering(Request $req)
     {
         try {
-            dd($req);
 
-            return response()->json(['status' => 200, 'message' => 'Successfully placed an Order for MFC']);
+            $validatedData = $req->validate([
+                'no_of_copies' => 'required|integer',
+                'firstname' => 'required|string|max:255',
+                'email' => 'required|email|unique:order_presale_for_locals,email',
+                'phone' => 'required|string|max:255',
+                'city' => 'required|string|max:255',
+            ]);
+
+            $message = OrderPresaleForLocal::create($validatedData);
+            Mail::to($req->email)->send(new OrderPresale($message));
+            
+            return response()->json(['status' => 200, 'message' => 'Successfully placed an Order for MFC Presale']);
             
         } catch (Throwable $th) {
             return $th->getMessage();
         }
     }
+
+
 }
---
+++ database/migrations/2025_08_01_060024_create_order_presale_for_locals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_presale_for_locals', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('city');
+            $table->integer('no_of_copies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_presale_for_locals');
+    }
+};
