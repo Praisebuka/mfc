81d8ad99acea0c4dccfffa06ca1ebef7404a8dda
Praisebuka <praise.njoga@gmail.com>
2025-08-01 13:49:53 +0100

    tried modifiing web to work - err still persist

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,23 @@ use App\Mail\SendQuickMSG;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json([ 'status' => 'success', 'message' => 'MFC Version 1.0', 'data' => [ 'v1' => url('v1/'), 's' => $_SERVER['REMOTE_ADDR'], ]]);
-});
+// Route::get('/', function () {
+//     return response()->json([ 'status' => 'success', 'message' => 'MFC Version 1.0', 'data' => [ 'v1' => url('v1/'), 's' => $_SERVER['REMOTE_ADDR'], ]]);
+// });
 
 Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
 
-Route::prefix('/v1')->group(function () {
-    Route::prefix('/orders')->group(function () {
-        Route::get('/global', [OrderBookController::class, 'globalOrdering'])->name('order globally');
-        Route::post('/local', [OrderBookController::class, 'localOrdering'])->name('order locally');
-        Route::post('/donate', [OrderBookController::class, 'donateCopies'])->name('donate a copy');
-    });
+// Route::prefix('/v1')->group(function () {
+//     Route::prefix('/orders')->group(function () {
+//         Route::get('/global', [OrderBookController::class, 'globalOrdering'])->name('order globally');
+//         Route::post('/donate', [OrderBookController::class, 'donateCopies'])->name('donate a copy');
+//         Route::post('/local', [OrderBookController::class, 'localOrdering'])->name('order locally');
+//     });
 
-    Route::post('/book-session', [OrderBookController::class, 'bookASession'])->name('book a session');
+//     Route::post('/book-session', [OrderBookController::class, 'bookASession'])->name('book a session');
 
 
-});
+// });
 
 
 
