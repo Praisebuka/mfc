7b377e53a985f6d8be98060ba882bb9d86fc35d4
Praisebuka <praise.njoga@gmail.com>
2025-09-26 20:52:49 +0100

    seperate table for timer pple newsletter

---
+++ app/Models/CountDownNewsletter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CountDownNewsletter extends Model
+{
+    protected $guarded = [];
+
+    protected $table = "count_down_newsletters";
+
+}
