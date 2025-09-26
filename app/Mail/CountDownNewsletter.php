8be728c61fdf7acf1bff87e3cf9c70b0df8f913e
Praisebuka <praise.njoga@gmail.com>
2025-09-26 20:55:24 +0100

    added mail for newsletter

---
+++ app/Mail/CountDownNewsletter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+
+class CountDownNewsletter extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Subscription Successful! - Welcome to My Family Companion 💙🎊',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'emails.newsletter',
+        );
+    }
+
+    public function attachments(): array
+    {
+        return [];
+    }
+
+}
+
